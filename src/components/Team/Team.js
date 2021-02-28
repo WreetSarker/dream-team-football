9555a318ea71a78236de6706a0c6870dc712fb1f
WreetSarker <wreet.sarker@gmail.com>
2021-03-01 01:02:41 +0600

    changed title name

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Team = (props) => {
     const salarySum = props.team.reduce((total, current) => total + parseFloat((current.salary).slice(1)), 0);
-    console.log(props.team);
     return (
         <div>
             <h2>Total added players: {props.team.length}</h2>
