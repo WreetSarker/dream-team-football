39832c42501b46ab10e454bbbc18f23afcbdf3dc
WreetSarker <wreet.sarker@gmail.com>
2021-03-01 00:53:43 +0600

    added bootstrap class

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Player.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const Player = (props) => {
-    const { name, position, email, phone, salary, image } = props.player;
+    const { name, position, club, phone, salary, image } = props.player;
     return (
         <div className="container-user">
             <div className="users-image">
@@ -12,11 +13,11 @@ const Player = (props) => {
             </div>
             <div className="users-detail">
                 <h3>Name: {name}</h3>
-                <h5>Email: {email}</h5>
+                <h5>Club: {club}</h5>
                 <p>Position: {position}</p>
                 <p>Phone: {phone}</p>
                 <p><small>Salary: {salary}</small></p>
-                <button className="add-btn" onClick={() => props.handleAddPlayers(props.player)}><FontAwesomeIcon icon={faPlusSquare} /> Add Player</button>
+                <button className="btn btn-warning" onClick={() => props.handleAddPlayers(props.player)}><FontAwesomeIcon icon={faPlusSquare} /> Add Player</button>
             </div>
         </div>
     );
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Team = (props) => {
-    const salarySum = props.team.reduce((total, current) => total + parseFloat((current.salary).slice(1)), 0)
+    const salarySum = props.team.reduce((total, current) => total + parseFloat((current.salary).slice(1)), 0);
+    console.log(props.team);
     return (
         <div>
             <h2>Total added players: {props.team.length}</h2>
-            <p>Total salary of added friends: {salarySum}</p>
+            <ul>
+                {
+                    props.team.map(player => <li>{player.name}</li>)
+                }
+            </ul>
+            <p>Total salary of added players: {salarySum}</p>
         </div>
     );
 };
